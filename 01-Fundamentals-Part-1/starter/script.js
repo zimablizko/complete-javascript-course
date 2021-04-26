7e7e6fd1f415d61706f79bdea95630d8864fb767
zimablizko <nklv92@gmail.com>
2021-04-26 21:48:34 +0300

    chore: wip

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,15 @@
+/*
+let js = 'amazing';
+
+
+let PI = 3.1415;
+
+let country = "Pepegastan";
+let continent = "Memeland";
+let population = 9000;
+
+console.log(country + " " + continent + " " + population)
+console.log(typeof ("qqq" + (10+1)))
+console.log(typeof null)*/
+
+const test = 123;
