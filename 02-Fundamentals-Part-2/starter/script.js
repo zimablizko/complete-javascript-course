986c06014968f35f992b49df6c94028232730d42
zimablizko <nklv92@gmail.com>
2021-04-30 00:36:50 +0300

    progress 30.04

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -67,3 +67,112 @@ const bills = [125, 555, 44];
 const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 console.log(tips, total);*/
+
+/*
+const jonas = {
+    firstName: "Jonas",
+    lastName: "wwww",
+    job: "teacher",
+    birthYear: 1991,
+    friends: ['Michael', 'f', 'v'],
+    hasDriversLicense: true,
+
+/!*    calcAge: function () {
+        return 2037 - this.birthYear;
+    }*!/
+    calcAge: function () {
+        this.age = 2037 - this.birthYear;
+        return this.age;
+    },
+
+    getSummary: function () {
+        return `${this.firstName} is a ${this.calcAge()}-year old ${this.job} and he has ${this.hasDriversLicense ? 'a' : 'no'} driver license`
+    }
+}
+
+const nameKey = 'Name';
+console.log(jonas['first' + nameKey]);
+console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is ${jonas.friends[0]}`);
+
+console.log(jonas.calcAge());
+console.log(jonas.age);
+console.log(jonas.getSummary());*/
+
+/*
+const mark = {
+    name: 'Mark',
+    mass: 78,
+    height: 1.69,
+    getBMI: function () {
+        this.bmi = this.mass / this.height ** 2
+        return this.bmi;
+    }
+};
+
+const jonas = {
+    name: 'Jonas',
+    mass: 92,
+    height: 1.95,
+    getBMI: function () {
+        this.bmi = this.mass / this.height ** 2
+        return this.bmi;
+    }
+};
+
+if (mark.getBMI() > jonas.getBMI())
+    console.log(`${mark.name}'s BMI (${mark.bmi}) is higher than ${jonas.name}'s BMI (${jonas.bmi})`)
+else
+    console.log(`${jonas.name}'s BMI (${jonas.bmi}) is higher than ${mark.name}'s BMI (${mark.bmi})`)*/
+
+/*
+const friends = ['Biba', 'Boba', 'Booba', 1, true];
+const types = [];
+for (let i = 0; i < friends.length; i++) {
+    console.log(friends[i]);
+    types.push(typeof friends[i]);
+}
+console.log(types);
+
+const years = [1999, 2008, 1923, 2020];
+const ages = [];
+for (let i = 0; i<years.length; i++) {
+    ages.push(2037-years[i]);
+}
+console.log(ages);
+
+//only strings
+for (let i = 0; i < friends.length; i++) {
+    if (typeof friends[i] !== 'string') continue;
+    console.log(friends[i]);
+}
+//break with number
+for (let i = 0; i < friends.length; i++) {
+    if (typeof friends[i] === 'number') break;
+    console.log(friends[i]);
+}*/
+
+/*
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while (dice !== 6) {
+    console.log(`You rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+}*/
+
+const bills = [22,295,176,440,37,105,10,1100,86,52];
+const tips = [];
+const totals = [];
+const calcTip = bill => (bill >= 50 && bill <= 300) ? bill * 0.15 : bill * 0.2;
+const calcAverage = function (arr) {
+   let avg = 0;
+    for (let i = 0; i < arr.length; i++) {
+        avg += arr[i];
+    }
+    return avg / arr.length;
+}
+for (let i = 0; i < bills.length; i++) {
+    tips.push(calcTip(bills[i]));
+    totals.push(bills[i]+tips[i]);
+}
+console.log(tips, totals);
+console.log(calcAverage(tips), calcAverage(totals));
